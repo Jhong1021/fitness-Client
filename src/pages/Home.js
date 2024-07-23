3f714e754c31da0b52a6cef89907d3ebdf2854c8
joenetuban <joenetuban@gmail.com>
2024-07-23 11:42:09 +0800

    add background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,27 @@ import { Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
+  const backgroundImageStyle = {
+    backgroundImage: 'url(https://wallpapers.com/images/featured/fitness-2e1dufzgv4p5mtk6.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh', // Full viewport height
+    color: 'white', // Ensure text is readable
+    textAlign: 'center',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  };
 
-	return (
-		<>
-        <Row>
-            <Col className="p-4 text-center">
-                <h1>Welcome to our Fitness Tracker App</h1>
-                <p>Create, Update, Delete and View Our Fitness Activities</p>
-                <Link className="btn btn-primary" to={'/items'}>Check Our Fitness Activities</Link>
-            </Col>
-        </Row>
-		</>
-	)
+  return (
+    <Row style={backgroundImageStyle}>
+      <Col>
+        <h1>Welcome to our Fitness Tracker App</h1>
+        <p>Create, Update, Delete and View Our Fitness Activities</p>
+        <Link className="btn btn-primary" to={'/items'}>Check Our Fitness Activities</Link>
+      </Col>
+    </Row>
+  );
 }
